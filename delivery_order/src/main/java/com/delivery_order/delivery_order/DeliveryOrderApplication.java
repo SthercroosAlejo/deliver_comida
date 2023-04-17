abb132729b58392ee53e703ed9385b7c9b494f69
Toshiro Escobedo Correa <aescobedoc4@upao.edu.pe>
2023-04-16 19:05:54 -0500

    create proyect-spring

---
+++ delivery_order/src/main/java/com/delivery_order/delivery_order/DeliveryOrderApplication.java
@@ -0,0 +1,13 @@
+package com.delivery_order.delivery_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryOrderApplication.class, args);
+	}
+
+}
